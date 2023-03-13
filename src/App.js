73ce676df51fa9161117cd7f5e0450844bb119e9
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-03-13 09:56:59 -0700

    begin

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from './components/Home'
+
+function App() {
+    return (
+        <div className="App">
+            <header>Planner application</header>
+            <Home/>
+            <footer>Created by Sangmi Contact: sangmiyun0529@gmail.com</footer>
+        </div>
+    );
+}
+
+
+
+export default App;
+
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import { React, useState } from 'react'
+import "./style.css"
+
+export default function Home() {
+
+    return (
+        <div className='home'>
+            <div className='content'>
+            
+
+            </div>
+        </div>
+    )
+}

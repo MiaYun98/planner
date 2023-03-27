eae482c43c0478b1637ef5614d68b1704914cd41
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-03-27 11:24:15 -0700

    adding week

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,15 @@ export default function Home() {
                 <h1>Study Planner</h1>
                 <h2>Weekly</h2>
             </div>
+            <div className='week'> 
+                <h2>Monday</h2>
+                <h2>Tuesday</h2>
+                <h2>Wendsday</h2>
+                <h2>Thursday</h2>
+                <h2>Friday</h2>
+                <h2>Saturday</h2>
+                <h2>Sunday</h2>
+            </div>
         </div>
     )
 }

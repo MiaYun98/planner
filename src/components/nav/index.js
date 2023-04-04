3f938f93604dd90cb185e9abe50595228a074bb4
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-04-04 10:22:37 -0700

    navbar edited

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -3,9 +3,13 @@ import "./style.css"
 export default function Navbar () {
     return (
         <div className="navbar">
-            <a href="/year"><h2>Year</h2></a>
-            <a href="/month"><h2>Month</h2></a>
-            <a href="/"><h2>Week</h2></a>
+            <ul>
+                <li><a href="/year"><h2>Year</h2></a></li>
+                <li><a href="/month"><h2>Month</h2></a></li>
+                <li><a href="/"><h2>Week</h2></a></li>
+                <li><a href="/"><h2>Profile</h2></a></li>
+            </ul>
+           
         </div>
     )
 }

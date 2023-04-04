0d56b4e390b46d21beb09de895f951d2400815eb
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-04-04 10:46:53 -0700

    should choose bootstrap vs css flex box

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,45 +9,31 @@ export default function Home() {
                 <h1>Study Planner</h1>
                 <h2>Weekly</h2>
             </div>
-            <div className='weekday'> 
-            {/* weekdays form it should be aline in a line */}
-                <ul className='weekday'>
-                    <li>
-                        <div className='card'>
-                            <h2>Monday</h2>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='card'>
-                            <h2>Tuesday</h2>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='card'>
-                            <h2>Wednesday</h2>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='card'>
-                            <h2>Thursday</h2>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='card'>
-                            <h2>Friday</h2>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='card'>
-                            <h2>Saturday</h2>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='card'>
-                            <h2>Sunday</h2>
-                        </div>
-                    </li>
-                </ul>
+            <div className='container text-center'>
+                <div className='weekday row'> 
+                {/* weekdays form it should be aline in a line */}
+                    <div className='card col'>
+                        <h2>Monday</h2>
+                    </div>
+                    <div className='card col'>
+                        <h2>Tuesday</h2>
+                    </div>
+                    <div className='card col'>
+                        <h2>Wednesday</h2>
+                    </div>
+                    <div className='card col'>
+                        <h2>Thursday</h2>
+                    </div>
+                    <div className='card col'>
+                        <h2>Friday</h2>
+                    </div>
+                    <div className='card col'>
+                        <h2>Saturday</h2>
+                    </div>
+                    <div className='card col'>
+                        <h2>Sunday</h2>
+                </div>
+            </div>
             </div>
         </div>
     )
